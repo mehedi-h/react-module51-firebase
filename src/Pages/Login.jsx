901db84adac72d55112d39fe87180b4d9cb4c7f7
mehedi-h <mehedihs2015@gmail.com>
2023-10-02 00:01:45 +0600

    create registration page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -8,17 +8,12 @@ const Login = () => {
         const password = e.target.password.value;
         console.log(email, password);
     }
+    
     return (
-    <div>
       <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
+        <div className="hero-content flex-col">
+          <div className="text-center md:py-3 lg:py-5">
             <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
@@ -65,7 +60,6 @@ const Login = () => {
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
---
+++ src/__context.api.js
@@ -0,0 +1,8 @@
+/*
+
+1. Create context api and export
+2. Provider
+3. set provider value.
+4. useContext to access context value.
+
+*/
